a5e836e260a2fb8573f9f52833efd3f32ca42c94
Bobby Brockway <bobby.brockway@gmail.com>
2019-10-16 10:01:01 +0100

    first integration test complete

--- hack/test-kubelock-kind.sh
+++ hack/test-kubelock-kind.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 BINARY_NAME="kubelock"
-DOCKER_REGISTRY="mintel"
 KIND_CONTEXT="kubernetes-admin@kind"
 TEST_REPLICAS=3
+EXITSTATUS=0
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -38,60 +38,71 @@ else
   echo "INFO: ${BINARY_NAME} namespace exists"
 fi
 
-# Load the docker image into kind
-
-#kind load docker-image ${DOCKER_REGISTRY}/${BINARY_NAME}:ci --loglevel debug
-
 # Apply the manifests
 
 kubectl apply -f "${THISDIR}"/manifests
 
 # Wait for endpoint annotation
+
 annotation=$(get_annotation)
+echo -n "INFO: waiting for annotation"
 while [[ $annotation == "null" ]]; do
-  echo "INFO: waiting for annotation"
+  echo -n "."
   sleep 1
   annotation=$(get_annotation)
 done
+echo
 
 function check_terminated {
   local statuses && statuses=$1
-  local maxTerminated && maxTerminated=$2
+  local expected && expected=$2
   terminated=0
   for status in $statuses; do
     if [[ $status == 'terminated' ]]; then
-      terminated++
+      ((terminated++))
     fi
   done
-  if [[ $terminated -gt $maxTerminated ]]; then
+  if [[ $terminated != "$expected" ]]; then
     return 1
   else
     return 0
   fi
 }
 
+jobsCompleted=0
 refresh_annotation_vars
 while true; do
 
-  echo "holderIdentity: $holderIdentity"
-  echo "leaseDurationSeconds: $leaseDurationSeconds"
-  echo "acquireTime: $acquireTime"
-  echo "renewTime: $renewTime"
-  echo "leaderTransitions: $leaderTransitions"
+  echo "DEBUG: holderIdentity: $holderIdentity"
+  echo "DEBUG: leaseDurationSeconds: $leaseDurationSeconds"
+  echo "DEBUG: acquireTime: $acquireTime"
+  echo "DEBUG: renewTime: $renewTime"
+  echo "DEBUG: leaderTransitions: $leaderTransitions"
 
   if [[ $holderIdentity != "" ]]; then
+    echo "INFO: lock obtained"
+
     while [[ $holderStatus != "terminated" ]]; do
       getPods=$(kubectl -n kubelock get pods -o json)
       holderStatus=$(echo "$getPods" | jq -r '.items[] | select(.metadata.name | contains("'"$holderIdentity"'")) | .status.initContainerStatuses[].state | keys[0]')
-      echo "holderStatus: $holderStatus"
+      #echo "DEBUG: holderStatus: $holderStatus"
       allStatuses=$(echo "$getPods" | jq -r '.items[] | .status.initContainerStatuses[].state | keys[0]')
-      echo "allStatuses: $allStatuses"
+      #echo "DEBUG: allStatuses: $allStatuses"
       sleep 1
     done
+
+    ((jobsCompleted++))
+
+    echo "=== RUN   TestTerminatedPods: $jobsCompleted/$TEST_REPLICAS"
+    if ! check_terminated "$allStatuses" "$leaderTransitions"; then
+      echo "FAIL: expected allStatuses: $leaderTransitions, leaderTransitions: $leaderTransitions, got allStatuses: $allStatuses, leaderTransitions: $leaderTransitions"
+      EXITSTATUS=1
+    else
+      echo "--- PASS: TestTerminatedPods: $jobsCompleted/$TEST_REPLICAS"
+    fi
+
     holderStatus=""
   fi
-
-  echo "---"
 
   if [[ $leaderTransitions -eq $TEST_REPLICAS ]]; then
     break
@@ -105,3 +116,5 @@ done
 # Clean up
 
 kubectl delete namespace kubelock
+
+exit $EXITSTATUS
